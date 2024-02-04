d70a4d34261bb89e4529b1d2389137262a56a750
Anjali24-54 <155923819+Anjali24-54@users.noreply.github.com>
2024-02-04 17:45:25 -0500

    Update WhileLoop.java

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,1 +1,12 @@
+public class WhileLoop {
 
+	public static void main(String [] args) {
+	
+		int count=1;
+		
+		while ( count < 10) {
+		System.out.println("count is " + count);
+		count++;
+		}
+	}
+}
